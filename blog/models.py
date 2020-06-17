6b4855691ccbd82271d755cd9260da0151cb7a35
RajKotadia <rajkotadia15@gmail.com>
2020-06-17 22:19:59 +0530

    :sparkles: Post model registered

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+	name = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.name
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_published = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.title
