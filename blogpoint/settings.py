3d2928f19f8d63712ee21710f2d38d5fdb7f398b
RajKotadia <rajkotadia15@gmail.com>
2020-06-21 21:18:21 +0530

    :sparkles: User registration logic

--- blogpoint/settings.py
+++ blogpoint/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
