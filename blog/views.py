d59b2a89668846483f6e5c11eb0466c53e9bcffc
RajKotadia <rajkotadia15@gmail.com>
2020-06-27 19:42:06 +0530

    :lipstick: Updated the UI

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 
 
 def home(request):
 
-	posts = Post.objects.all()
+	posts = Post.objects.all().order_by('-date_published')
+	tags = Tag.objects.all()
 
 	context = {
-		'posts': posts
+		'posts': posts,
+		'tags': tags,
 	}
 	return render(request, 'blog/index.html', context)
+
+
+def about(request):
+	return render(request, 'blog/about.html')
