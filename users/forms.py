1f8eec2b5e6273f23b6bf150454927c28bedcce6
RajKotadia <rajkotadia15@gmail.com>
2020-06-27 19:43:56 +0530

    :sparkles: User Profile update logic

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -13,4 +14,28 @@ class UserRegisterForm(UserCreationForm):
 		widgets = {
 			'username': forms.TextInput(attrs={'autocomplete':'off'}),
 			'email': forms.EmailInput(attrs={'autocomplete':'off'}),
+		}
+
+
+class UserUpdateForm(forms.ModelForm):
+	first_name = forms.CharField()
+	last_name = forms.CharField()
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email')
+
+		widgets = {
+			'username': forms.TextInput(attrs={'autocomplete':'off'}),
+			'email': forms.EmailInput(attrs={'autocomplete':'off'}),
+		}
+
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['profile_image', 'bio']
+		widgets = {
+			'bio': forms.TextInput(attrs={'autocomplete':'off'}),
 		}
--- users/views.py
+++ users/views.py
@@ -2,16 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-
+# to register a new user into the system
 def register(request):
-
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)
 
 		if form.is_valid():
-			print(form.cleaned_data)
 			username = form.cleaned_data.get('username')
 			form.save()
 			messages.success(request, f'Account created for "{username}" !') 
@@ -23,7 +21,37 @@ def register(request):
 	return render(request, 'users/register.html', {'form': form})
 
 
-# protected route/view
+# profile / update profile view
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html', {})
+	# handle form submission
+	if request.method == 'POST':
+		user_form = UserUpdateForm(request.POST, instance=request.user)
+		profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, f'Profile successfully updated !') 
+			return redirect('profile')
+
+	else:
+		# populate the forms with current data
+		user_form = UserUpdateForm(instance=request.user)
+		profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+		posts = request.user.post_set.all()
+		tags = set([ tag.name for p in posts for tag in p.tags.all() ])
+		
+
+	context = {
+		'user_form': user_form,
+		'profile_form': profile_form,
+		'posts': posts,
+		'tags': tags
+	}
+
+	print(user_form.errors)
+	print(profile_form.errors)
+
+	return render(request, 'users/profile.html', context)
